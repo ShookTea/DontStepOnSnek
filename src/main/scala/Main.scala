6c25911af30f2e499bc40c1f62a0a78dc0580904
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-01-12 18:30:17 +0100

    Random movement

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,15 @@ package eu.shooktea.dsos
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    var board = Board(50, 15)
+
+    for (_ <- 1 to 100) {
+      println(board)
+      board = Utils.random.nextInt(3) match {
+        case 0 => board.moveForward()
+        case 1 => board.moveRight()
+        case 2 => board.moveLeft()
+      }
+    }
   }
 }
