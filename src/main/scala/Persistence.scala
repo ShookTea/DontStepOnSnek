042eff00155278144bacf03451302bc555dfe6ae
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-01-14 15:18:14 +0100

    Storing all data of neural networks

--- src/main/scala/Persistence.scala
+++ src/main/scala/Persistence.scala
@@ -2,6 +2,7 @@ package eu.shooktea.dsos
 
 import java.io.{DataOutputStream, FileOutputStream}
 import java.nio.file.Paths
+import java.util.UUID
 
 object Persistence {
   def save(networkClass: NetworkClass, path: String): Unit = {
@@ -23,5 +24,19 @@ object Persistence {
   private def save(nc: NetworkClass, dos: DataOutputStream): Unit = {
     dos.writeInt(nc.generation)
     val best = nc.getBest
+    dos.writeInt(best.length)
+
+    best.foreach(saveResultEntry(dos))
+  }
+
+  private def saveResultEntry(dos: DataOutputStream)(entry: (NeuralNetwork, TestResult)) : Unit = entry match {
+    case (nn: NeuralNetwork, _) =>
+      val uuid = UUID.fromString(nn.identifier)
+      dos.writeLong(uuid.getMostSignificantBits)
+      dos.writeLong(uuid.getLeastSignificantBits)
+      dos.writeInt(nn.generation)
+
+      dos.writeInt(nn.weights.length)
+      nn.weights.foreach(dos.writeDouble)
   }
 }
