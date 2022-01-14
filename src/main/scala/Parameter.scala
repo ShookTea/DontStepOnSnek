5559957398105434536e2ebfb27117f21fb9b565
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-01-14 16:19:40 +0100

    Adjusting parameters

--- src/main/scala/Parameter.scala
+++ src/main/scala/Parameter.scala
@@ -2,7 +2,7 @@ package eu.shooktea.dsos
 
 object Parameter {
   /** How many boards does each NN get to make a grade? */
-  val testIterations: Int = 5
+  val testIterations: Int = 20
 
   /** Map sizes */
   val mapWidth: Int = 50
@@ -14,14 +14,14 @@ object Parameter {
   val movePointsPerFood: Int = 90
 
   /** How many best NNs graduate to next class? */
-  val graduationCount: Int = 12
+  val graduationCount: Int = 15
   /** How many mutated clones are created for each graduated NN? */
-  val mutationCount: Int = 4
+  val mutationCount: Int = 40
   /** How many new networks must be randomly generated on start of a round? */
   val classSize: Int = ((mutationCount + 1) * graduationCount * (graduationCount + 1)) / 2
 
   /** Weights of test results for final grade */
-  val foodWeight: Double = 45.0
+  val foodWeight: Double = 9.0
   val remainingMovesWeight: Double = 0.1
   val movesGoingAwayWeight: Double = -0.25
 }
