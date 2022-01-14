9f48f14f17de8957a372d7ef0f6ce49dafc20a21
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-01-14 12:57:43 +0100

    Creating data output stream

--- src/main/scala/Persistence.scala
+++ src/main/scala/Persistence.scala
@@ -1,16 +1,24 @@
 package eu.shooktea.dsos
 
+import java.io.{DataOutputStream, FileOutputStream}
 import java.nio.file.Paths
 
 object Persistence {
   def save(networkClass: NetworkClass, path: String): Unit = {
     val file = Paths.get(path).toFile
-    if (file.exists()) {
-      println("EXISTS!")
+    print("Saving...")
+    if (file.exists())
       file.delete()
-    } else {
-      println("CREATING FILE!")
+    else
       file.createNewFile()
-    }
+
+    val dos = new DataOutputStream(
+      new FileOutputStream(file)
+    )
+    save(networkClass, dos)
+    dos.close()
+    println(" done.")
   }
+
+  private def save(nc: NetworkClass, dos: DataOutputStream): Unit = {}
 }
